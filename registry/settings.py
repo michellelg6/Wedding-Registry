31a4a0a6b7487b4456168f2a44b12efcd29e3502
Michelle Gepshtein <michelle.liran@gmail.com>
2021-02-19 15:13:09 -0800

    added all the rest of the files

--- registry/settings.py
+++ registry/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_FILES = [ ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -132,3 +133,4 @@ try:
     from .local_settings import *
 except ImportError:   
     print("Looks like no local file! You must be on production!")
+    
